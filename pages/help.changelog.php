bb273b8d7946560c7f315618f2fb6a9a0be7014c
Tobias Krais <tk@design-to-use.de>
2026-04-29 23:56:42 +0200

    ergaenze CSRF-Schutz für Modul-Installation und -Aktionen im Backend

--- pages/help.changelog.php
+++ pages/help.changelog.php
@@ -4,7 +4,7 @@
 <p>1.3.1-DEV:</p>
 <ul>
 	<li>Backend: CSRF-Schutz fuer Speichern-, Loesch- und Statusaktionen der Stellenverwaltung sowie fuer den HR4YOU-Import ergaenzt.</li>
-	<li>...</li>
+	<li>Backend: CSRF-Schutz fuer Modul-Installation, -Update und -Deinstallation auf der Setup-Seite ergaenzt.</li>
 </ul>
 <p>1.3.0:</p>
 <ul>
