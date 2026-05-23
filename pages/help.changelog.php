41e99e404a6fd00df77cffe020e067fafffc4826
Tobias Krais <tk@design-to-use.de>
2026-05-23 20:08:26 +0200

    abort button leads to list

--- pages/help.changelog.php
+++ pages/help.changelog.php
@@ -3,6 +3,7 @@
 <h2>Changelog</h2>
 <p>1.3.1-DEV:</p>
 <ul>
+	<li>Backend: Abbrechen-Buttons in Stellen-, Kategorien- und Kontaktformularen fuehren jetzt wieder zur Liste.</li>
 	<li>Backend: CSRF-Schutz fuer Speichern-, Loesch- und Statusaktionen der Stellenverwaltung sowie fuer den HR4YOU-Import ergaenzt.</li>
 	<li>Backend: CSRF-Schutz fuer Modul-Installation, -Update und -Deinstallation auf der Setup-Seite ergaenzt.</li>
 	<li>Sicherheit: Job-, Category- und Contact-Modelle speichern jetzt mit gebundenen Parametern. Die bisherigen addslashes()-Aufrufe und String-Konkatenationen in Save- und Suchqueries entfallen damit.</li>
